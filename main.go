908319eecf9a60a8344514ff0bc76b7f9738c3d8
benoitbMTL <benoit@buonassera.fr>
2023-12-24 14:34:20 -0500

    middleware.Logger

--- main.go
+++ main.go
@@ -11,7 +11,9 @@ func main() {
 	e := echo.New()
 
 	// Middleware
-	e.Use(middleware.Logger())
+	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
+		Format: "method=${method}, uri=${uri}, status=${status}\n",
+	}))
 	e.Use(middleware.Recover())
 
 	// Serve static files
