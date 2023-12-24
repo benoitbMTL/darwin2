68be4d0a3279231469aefc5c62b7469196345a3b
benoitbMTL <benoit@buonassera.fr>
2023-12-24 14:30:22 -0500

    app.js

--- main.go
+++ main.go
@@ -1,24 +1,27 @@
 package main
 
 import (
-    "net/http"
-    "github.com/labstack/echo/v4"
-    "github.com/labstack/echo/v4/middleware"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"net/http"
 )
 
 func main() {
-    // Create a new Echo instance
-    e := echo.New()
+	// Create a new Echo instance
+	e := echo.New()
 
-    // Middleware
-    e.Use(middleware.Logger())
-    e.Use(middleware.Recover())
+	// Middleware
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
 
-    // Route to handle the root request
-    e.GET("/", func(c echo.Context) error {
-        return c.String(http.StatusOK, "Welcome to the backend server!")
-    })
+	// Serve static files
+	e.Static("/", "public")
 
-    // Start the server on port 8080
-    e.Logger.Fatal(e.Start(":8080"))
+	// Route to handle version request
+	e.GET("/version", func(c echo.Context) error {
+		return c.String(http.StatusOK, "(c) Darwin 2.0 (2024)")
+	})
+
+	// Start the server on port 8080
+	e.Logger.Fatal(e.Start(":8080"))
 }
