9e52a48aa5b0ab0953dbde93b161ec20eb591a64
BenoitbMTL <benoit@buonassera.fr>
2023-12-25 00:08:17 -0500

    test

--- vue/src/views/AboutView.vue
+++ vue/src/views/AboutView.vue
@@ -1,5 +1,12 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque harum aliquam, eum minus voluptate odio
+      distinctio, aliquid velit consequuntur atque repudiandae similique sit sequi, corporis cupiditate blanditiis
+      necessitatibus quidem maiores sint doloremque debitis ipsa. Est a commodi neque, ipsam quisquam hic? Facilis, sunt
+      rem! Ea, architecto ratione modi praesentium voluptates dolore molestiae accusamus. Doloremque culpa eos quasi!
+      Commodi voluptates dolorem a esse eaque nesciunt ea veniam blanditiis. Minus incidunt doloribus esse reprehenderit
+      facere quisquam delectus quos, illo culpa fuga veniam minima odio recusandae. Excepturi sed accusantium rerum
+      dolorum eius odit, libero accusamus impedit ab in magnam vel nulla natus ducimus?</p>
   </div>
 </template>
--- vue/src/views/HomeView.vue
+++ vue/src/views/HomeView.vue
@@ -1,18 +1,25 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
-</template>
+    <h1>FortiWeb Demo Tool</h1>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque harum aliquam, eum minus voluptate odio
+      distinctio, aliquid velit consequuntur atque repudiandae similique sit sequi, corporis cupiditate blanditiis
+      necessitatibus quidem maiores sint doloremque debitis ipsa. Est a commodi neque, ipsam quisquam hic? Facilis, sunt
+      rem! Ea, architecto ratione modi praesentium voluptates dolore molestiae accusamus. Doloremque culpa eos quasi!
+      Commodi voluptates dolorem a esse eaque nesciunt ea veniam blanditiis. Minus incidunt doloribus esse reprehenderit
+      facere quisquam delectus quos, illo culpa fuga veniam minima odio recusandae. Excepturi sed accusantium rerum
+      dolorum eius odit, libero accusamus impedit ab in magnam vel nulla natus ducimus?</p>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque harum aliquam, eum minus voluptate odio
+      distinctio, aliquid velit consequuntur atque repudiandae similique sit sequi, corporis cupiditate blanditiis
+      necessitatibus quidem maiores sint doloremque debitis ipsa. Est a commodi neque, ipsam quisquam hic? Facilis, sunt
+      rem! Ea, architecto ratione modi praesentium voluptates dolore molestiae accusamus. Doloremque culpa eos quasi!
+      Commodi voluptates dolorem a esse eaque nesciunt ea veniam blanditiis. Minus incidunt doloribus esse reprehenderit
+      facere quisquam delectus quos, illo culpa fuga veniam minima odio recusandae. Excepturi sed accusantium rerum
+      dolorum eius odit, libero accusamus impedit ab in magnam vel nulla natus ducimus?</p>
+  </div></template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
-  }
+  components: { }
 }
 </script>
