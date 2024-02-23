322a0373e51aeb71a9b5c10b4ed20744abd75d33
BenoitbMTL <benoit@buonassera.fr>
2024-02-23 12:51:38 -0500

    debug

--- go/main.go
+++ go/main.go
@@ -1,13 +1,14 @@
 package main
 
 import (
-	"darwin2/config"  // Import the config package
+	"darwin2/config" // Import the config package
 	"darwin2/routes"
-	"github.com/labstack/echo/v4"
-	"github.com/labstack/echo/v4/middleware"
 	"io/ioutil"
 	"net/http"
 	"path/filepath"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
@@ -18,8 +19,13 @@ func main() {
 	e := echo.New()
 
 	// Middleware
+	// e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
+	// 	Format: "${method}, ${uri}, ${status}\n",
+	// }))
 	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
-		Format: "${method}, ${uri}, ${status}\n",
+		Format: "time=${time_rfc3339}, method=${method}, uri=${uri}, status=${status}, " +
+			"latency=${latency_human}, latency_ns=${latency}, bytes_in=${bytes_in}, " +
+			"bytes_out=${bytes_out}, remote_ip=${remote_ip}, error=${error}\n",
 	}))
 	e.Use(middleware.Recover())
 	e.Use(middleware.CORS())
