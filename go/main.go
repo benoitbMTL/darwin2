bf7acd98a02489639f598278710be6440e44a330
BenoitbMTL <benoit@buonassera.fr>
2024-02-23 13:21:00 -0500

    mode debug

--- go/main.go
+++ go/main.go
@@ -1,8 +1,9 @@
 package main
 
 import (
-	"darwin2/config" // Import the config package
+	"darwin2/config"
 	"darwin2/routes"
+	"flag"
 	"io/ioutil"
 	"net/http"
 	"path/filepath"
@@ -11,41 +12,51 @@ import (
 	"github.com/labstack/echo/v4/middleware"
 )
 
+var debugMode bool
+
+func init() {
+	flag.BoolVar(&debugMode, "debug", false, "Enable debug mode")
+}
+
 func main() {
-	// Initialize the application configuration
+	flag.Parse() // Parse any command-line flags
+
 	config.Initialize()
 
-	// Create a new Echo instance
 	e := echo.New()
 
-	// Middleware
-	// e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
-	// 	Format: "${method}, ${uri}, ${status}\n",
-	// }))
-	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
-		Format: "time=${time_rfc3339}, method=${method}, uri=${uri}, status=${status}, " +
-			"latency=${latency_human}, latency_ns=${latency}, bytes_in=${bytes_in}, " +
-			"bytes_out=${bytes_out}, remote_ip=${remote_ip}, error=${error}\n",
-	}))
+	// Enabling debug mode based on the debug flag
+	e.Debug = debugMode
+
+	// Adjust logging based on debug mode
+	if debugMode {
+		// When in debug mode, use the default logger for simplicity
+		e.Logger.Info("Debug mode enabled")
+		e.Use(middleware.Logger())
+	} else {
+		e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
+			Format: "time=${time_rfc3339}, method=${method}, uri=${uri}, status=${status}, " +
+				"latency=${latency_human}, latency_ns=${latency}, bytes_in=${bytes_in}, " +
+				"bytes_out=${bytes_out}, remote_ip=${remote_ip}, error=${error}\n",
+		}))
+	}
+
 	e.Use(middleware.Recover())
 	e.Use(middleware.CORS())
 
-	// Routes
 	routes.Configure(e)
 
-	// Vue Production Build
-	e.Static("/", "../vue/dist") // Adjust the path to your Vue app's dist directory as necessary
+	e.Static("/", "../vue/dist")
 
-	// Catch-all Route to Serve index.html
 	e.GET("/*", func(c echo.Context) error {
-		indexPath := filepath.Join("../vue/dist", "index.html") // Adjust the path as necessary
+		indexPath := filepath.Join("../vue/dist", "index.html")
 		indexFile, err := ioutil.ReadFile(indexPath)
 		if err != nil {
-			return err // Or return an appropriate error in your context
+			e.Logger.Errorf("Failed to serve index.html: %v", err)
+			return err
 		}
 		return c.HTMLBlob(http.StatusOK, indexFile)
 	})
 
-	// Start server on port 8080
 	e.Logger.Fatal(e.Start(":8080"))
 }
