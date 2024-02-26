9fbf03b5af65504824e1ad44ca13fb93a019cb0c
benoitbMTL <benoit@buonassera.fr>
2024-02-25 23:25:54 -0500

    test .env

--- vue/src/components/05-tool/ConfigForm.vue
+++ vue/src/components/05-tool/ConfigForm.vue
@@ -218,7 +218,7 @@ export default {
   methods: {
     submitForm() {
       // Implement API call to update configuration
-      fetch("http://localhost:8080/config", {
+        fetch(`${process.env.VUE_APP_BACKEND_URL}/config`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
@@ -242,7 +242,7 @@ export default {
         });
     },
     resetConfig() {
-      fetch("http://localhost:8080/reset")
+      fetch(`${process.env.VUE_APP_BACKEND_URL}/reset`)
         .then((response) => response.json())
         .then((data) => {
           this.showAlert = true;
@@ -264,7 +264,7 @@ export default {
   mounted() {
     // Fetch current configuration from the Go backend
     console.log("Making a GET request to http://localhost:8080/config");
-    fetch("http://localhost:8080/config")
+    fetch(`${process.env.VUE_APP_BACKEND_URL}/config`)
       .then((response) => {
         console.log("HTTP return code:", response.status); // Print HTTP return code
 
