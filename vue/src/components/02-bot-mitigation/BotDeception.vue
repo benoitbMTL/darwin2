5454c9536a3b5939b92ccb578c072ba2078d5f9c
benoitbMTL <benoit@buonassera.fr>
2024-03-02 23:36:18 -0500

    test

--- vue/src/components/02-bot-mitigation/BotDeception.vue
+++ vue/src/components/02-bot-mitigation/BotDeception.vue
@@ -15,7 +15,7 @@
       </p>
 
       <button class="btn btn-primary btn-sm me-2" @click="viewPageSource">
-        View Page Source
+        View Page Source AAAAA
       </button>
       <button class="btn btn-primary btn-sm me-2" @click="performBotDeception">
         Run Deception
