b3c9636823e5a83d4b333cbacef329e5744bc750
benoitbMTL <benoit@buonassera.fr>
2024-03-04 00:42:43 -0500

    selenium/chromedriver

--- go/handlers/botScraperSelenium.go
+++ go/handlers/botScraperSelenium.go
@@ -24,7 +24,7 @@ const Loop = 300
 // https://googlechromelabs.github.io/chrome-for-testing/
 // https://storage.googleapis.com/chrome-for-testing-public/122.0.6261.94/linux64/chromedriver-linux64.zip
 const (
-	chromeDriverPath = "./selenium/chromedriver" // Path to ChromeDriver
+	chromeDriverPath = "selenium/chromedriver" // Path to ChromeDriver
 	port             = 4444                      // Port on which ChromeDriver will listen
 )
 
