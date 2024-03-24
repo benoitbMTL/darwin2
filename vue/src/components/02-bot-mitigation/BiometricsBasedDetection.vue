f5439175c41cb9e6f17cb16de10d14feb4515638
benoitbMTL <benoit@buonassera.fr>
2024-03-24 03:16:29 -0400

    new options

--- vue/src/components/02-bot-mitigation/BiometricsBasedDetection.vue
+++ vue/src/components/02-bot-mitigation/BiometricsBasedDetection.vue
@@ -9,76 +9,101 @@
     </div>
 
     <div class="card-body">
-      <label class="form-label">Automated Web Interactions on Juice Shop with Selenium. Choose Your Actions:</label>
 
-      <div class="card" style="width: 18rem;">
-        <ul class="list-group list-group-flush">
-          <li class="list-group-item">
-            <div class="form-check">
-              <input class="form-check-input" type="checkbox" value="clickAllProducts" id="clickAllProducts"
-                v-model="selectedActions">
-              <label class="form-check-label" for="clickAllProducts">View All Products</label>
+      <div class="d-flex justify-content-between">
+        <div>
+
+          <label class="form-label">Automated Web Interactions on Juice Shop with Selenium. Choose Your Actions:</label>
+
+          <div class="card" style="width: 18rem;">
+            <ul class="list-group list-group-flush">
+              <li class="list-group-item">
+                <div class="form-check">
+                  <input class="form-check-input" type="checkbox" value="clickAllProducts" id="clickAllProducts"
+                    v-model="selectedActions">
+                  <label class="form-check-label" for="clickAllProducts">View All Products</label>
+                </div>
+              </li>
+              <li class="list-group-item">
+                <div class="form-check">
+                  <input class="form-check-input" type="checkbox" value="createAccount" id="createAccount"
+                    v-model="selectedActions">
+                  <label class="form-check-label" for="createAccount">Create Account</label>
+                </div>
+              </li>
+              <li class="list-group-item">
+                <div class="form-check">
+                  <input class="form-check-input" type="checkbox" value="login" id="login" v-model="selectedActions"
+                    :disabled="!selectedActions.includes('createAccount')">
+                  <label class="form-check-label" for="login">Login</label>
+                </div>
+              </li>
+              <li class="list-group-item">
+                <div class="form-check">
+                  <input class="form-check-input" type="checkbox" value="addNewAddress" id="addNewAddress"
+                    v-model="selectedActions" :disabled="!selectedActions.includes('login')">
+                  <label class="form-check-label" for="addNewAddress">Add New Address</label>
+                </div>
+              </li>
+              <li class="list-group-item">
+                <div class="form-check">
+                  <input class="form-check-input" type="checkbox" value="addNewPayment" id="addNewPayment"
+                    v-model="selectedActions" :disabled="!selectedActions.includes('login')">
+                  <label class="form-check-label" for="addNewPayment">Add New Payment</label>
+                </div>
+              </li>
+              <li class="list-group-item">
+                <div class="form-check">
+                  <input class="form-check-input" type="checkbox" value="addItemsToShoppingCart"
+                    id="addItemsToShoppingCart" v-model="selectedActions"
+                    :disabled="!selectedActions.includes('login')">
+                  <label class="form-check-label" for="addItemsToShoppingCart">Add Items to Shopping Cart</label>
+                </div>
+              </li>
+              <li class="list-group-item">
+                <div class="form-check">
+                  <input class="form-check-input" type="checkbox" value="checkoutShoppingCart" id="checkoutShoppingCart"
+                    v-model="selectedActions"
+                    :disabled="!selectedActions.includes('addNewAddress') || !selectedActions.includes('addNewPayment')">
+                  <label class="form-check-label" for="checkoutShoppingCart">Checkout Shopping Cart</label>
+                </div>
+              </li>
+              <li class="list-group-item">
+                <div class="form-check">
+                  <input class="form-check-input" type="checkbox" value="logout" id="logout" v-model="selectedActions"
+                    :disabled="!selectedActions.includes('login')">
+                  <label class="form-check-label" for="logout">Logout</label>
+                </div>
+              </li>
+            </ul>
+          </div>
+
+          <div class="mt-3">
+            <button class="btn btn-primary btn-sm me-2" @click="runCustomSelenium">Run Actions</button>
+            <button class="btn btn-secondary btn-sm me-2" @click="resetResult">Reset</button>
+            <span v-if="jobResult" class="result-message text-danger fw-bold">{{ jobResult }}</span>
+          </div>
+
+
+          <div>
+            <div class="card" style="width: 18rem;">
+              <ul class="list-group list-group-flush">
+                <li class="list-group-item">
+                  Number of loops
+                  <input type="number" class="form-control" v-model="loopCount" min="1" style="width: 100px;"
+                    placeholder="1">
+                </li>
+                <li class="list-group-item">
+                  <div class="form-check">
+                    <input class="form-check-input" type="checkbox" id="headless" v-model="isHeadless">
+                    <label class="form-check-label" for="headless">Headless</label>
+                  </div>
+                </li>
+              </ul>
             </div>
-          </li>
-          <li class="list-group-item">
-            <div class="form-check">
-              <input class="form-check-input" type="checkbox" value="createAccount" id="createAccount"
-                v-model="selectedActions">
-              <label class="form-check-label" for="createAccount">Create Account</label>
-            </div>
-          </li>
-          <li class="list-group-item">
-            <div class="form-check">
-              <input class="form-check-input" type="checkbox" value="login" id="login" v-model="selectedActions"
-                :disabled="!selectedActions.includes('createAccount')">
-              <label class="form-check-label" for="login">Login</label>
-            </div>
-          </li>
-          <li class="list-group-item">
-            <div class="form-check">
-              <input class="form-check-input" type="checkbox" value="addNewAddress" id="addNewAddress"
-                v-model="selectedActions" :disabled="!selectedActions.includes('login')">
-              <label class="form-check-label" for="addNewAddress">Add New Address</label>
-            </div>
-          </li>
-          <li class="list-group-item">
-            <div class="form-check">
-              <input class="form-check-input" type="checkbox" value="addNewPayment" id="addNewPayment"
-                v-model="selectedActions" :disabled="!selectedActions.includes('login')">
-              <label class="form-check-label" for="addNewPayment">Add New Payment</label>
-            </div>
-          </li>
-          <li class="list-group-item">
-            <div class="form-check">
-              <input class="form-check-input" type="checkbox" value="addItemsToShoppingCart" id="addItemsToShoppingCart"
-                v-model="selectedActions" :disabled="!selectedActions.includes('login')">
-              <label class="form-check-label" for="addItemsToShoppingCart">Add Items to Shopping Cart</label>
-            </div>
-          </li>
-          <li class="list-group-item">
-            <div class="form-check">
-              <input class="form-check-input" type="checkbox" value="checkoutShoppingCart" id="checkoutShoppingCart"
-                v-model="selectedActions"
-                :disabled="!selectedActions.includes('addNewAddress') || !selectedActions.includes('addNewPayment')">
-              <label class="form-check-label" for="checkoutShoppingCart">Checkout Shopping Cart</label>
-            </div>
-          </li>
-          <li class="list-group-item">
-            <div class="form-check">
-              <input class="form-check-input" type="checkbox" value="logout" id="logout" v-model="selectedActions"
-                :disabled="!selectedActions.includes('login')">
-              <label class="form-check-label" for="logout">Logout</label>
-            </div>
-          </li>
-        </ul>
+          </div>
+        </div>
       </div>
-
-      <div>
-        <button class="btn btn-primary btn-sm me-2" @click="runCustomSelenium">Run Actions</button>
-        <button class="btn btn-secondary btn-sm me-2" @click="resetResult">Reset</button>
-        <span v-if="jobResult" class="result-message text-danger fw-bold">{{ jobResult }}</span>
-      </div>
-
     </div>
   </div>
 
@@ -89,10 +114,12 @@
     </div>
     <div class="card-body">
       <p>By checking the client events such as mouse movement, keyboard, screen touch, and scroll, etc in specified
-        period, FortiWeb judges whether the request comes from a human or from a bot. You can configure the biometrics
+        period, FortiWeb judges whether the request comes from a human or from a bot. You can configure the
+        biometrics
         based detection rule to define the client event, collection period, and the request URL, etc.</p>
     </div>
   </div>
+
 </template>
 
 <script>
